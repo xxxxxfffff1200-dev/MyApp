13fe15946cf3df645e9fb6bef91a1d6202bb8373
xxxxxfffff1200-dev <xxxxxfffff1200@gmail.com>
2026-06-28 21:03:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from kivy.app import App
-from kivy.uix.button import Button
+jobs:
+  build:
+    runs-on: ubuntu-latest
 
-class MyApp(App):
-    def build(self):
-        return Button(text="Hello APK from GitHub")
+    steps:
+      - uses: actions/checkout@v4
 
-MyApp().run()
+      - name: Install dependencies
+        run: |
+          sudo apt update
+          sudo apt install -y python3-pip git zip unzip openjdk-17-jdk
+          pip install buildozer cython
+
+      - name: Accept Android licenses
+        run: yes | sdkmanager --licenses || true
+
+      - name: Build APK
+        run: buildozer android debug
